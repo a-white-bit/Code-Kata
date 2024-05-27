e04adbb3343f522c36730a9c7205614e6edfe190
Soyoung Yang <72538861+a-white-bit@users.noreply.github.com>
2024-05-27 09:34:27 +0900

    [level 1] Title: K번째수, Time: 3.46 ms, Memory: 80.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        List<Integer> answer = new ArrayList();
+        for (int[] command : commands) {
+            int[] arr = Arrays.copyOfRange(array, command[0]-1, command[1]);
+            Arrays.sort(arr);
+            answer.add(arr[command[2]-1]);
+        }
+        
+        return answer.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
